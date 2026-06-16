f3edba9d2c0a4c62cb1aa13f869d9106daf31469
Nethra <nethra.bagur@gmail.com>
2026-06-16 11:04:43 +0530

    eComm@1104

--- pages/Login.js
+++ pages/Login.js
@@ -3,9 +3,9 @@ import login_data from '../test_data/Login.json'
 export class login_loc{
     constructor(page){
         this.page=page
-        this.email=page.locator(`//input[@data-qa="login---------email"]`)
-        this.password=page.locator(`//input[@data-qa="login------------password"]`)
-        this.login_button=page.locator(`//button[@data-qa="login----------button"]`)
+        this.email=page.locator(`//input[@data-qa="login-email"]`)
+        this.password=page.locator(`//input[@data-qa="login-password"]`)
+        this.login_button=page.locator(`//button[@data-qa="login-button"]`)
     }
     async login_url() {
         await this.page.goto(login_data.URL)
